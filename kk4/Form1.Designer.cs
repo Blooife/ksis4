bbd6836a402173339f96a6d7d299708a71bc4f4d
Blooife <beusas.23@gmail.com>
2023-05-07 23:47:01 +0300

    Initial commit

---
+++ kk4/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace kk4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.butOk = new System.Windows.Forms.Button();
+            this.tbOut = new System.Windows.Forms.RichTextBox();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.Num = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ms = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dns = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.city = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.region = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.org = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemory = 5;
+            this.map.Location = new System.Drawing.Point(12, 192);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(1112, 396);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 0D;
+            this.map.Load += new System.EventHandler(this.gMapControl1_Load);
+            // 
+            // butOk
+            // 
+            this.butOk.Location = new System.Drawing.Point(142, 139);
+            this.butOk.Name = "butOk";
+            this.butOk.Size = new System.Drawing.Size(75, 23);
+            this.butOk.TabIndex = 1;
+            this.butOk.Text = "OK";
+            this.butOk.UseVisualStyleBackColor = true;
+            this.butOk.Click += new System.EventHandler(this.butOk_Click);
+            // 
+            // tbOut
+            // 
+            this.tbOut.Location = new System.Drawing.Point(223, 12);
+            this.tbOut.Name = "tbOut";
+            this.tbOut.Size = new System.Drawing.Size(224, 150);
+            this.tbOut.TabIndex = 2;
+            this.tbOut.Text = "";
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.Num, this.IP, this.ms, this.dns, this.city, this.region, this.org });
+            this.grid.Location = new System.Drawing.Point(453, 12);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(640, 150);
+            this.grid.TabIndex = 4;
+            // 
+            // Num
+            // 
+            this.Num.HeaderText = "Num";
+            this.Num.Name = "Num";
+            // 
+            // IP
+            // 
+            this.IP.HeaderText = "IP";
+            this.IP.Name = "IP";
+            // 
+            // ms
+            // 
+            this.ms.HeaderText = "ms";
+            this.ms.Name = "ms";
+            // 
+            // dns
+            // 
+            this.dns.HeaderText = "dns";
+            this.dns.Name = "dns";
+            // 
+            // city
+            // 
+            this.city.HeaderText = "city";
+            this.city.Name = "city";
+            // 
+            // region
+            // 
+            this.region.HeaderText = "region";
+            this.region.Name = "region";
+            // 
+            // org
+            // 
+            this.org.HeaderText = "org";
+            this.org.Name = "org";
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(99, 113);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(118, 20);
+            this.tbIP.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1136, 600);
+            this.Controls.Add(this.tbIP);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.tbOut);
+            this.Controls.Add(this.butOk);
+            this.Controls.Add(this.map);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox tbIP;
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn org;
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn dns;
+        private System.Windows.Forms.DataGridViewTextBoxColumn city;
+        private System.Windows.Forms.DataGridViewTextBoxColumn region;
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn Num;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ms;
+
+        private System.Windows.Forms.DataGridView grid;
+
+        private System.Windows.Forms.RichTextBox tbOut;
+
+        private System.Windows.Forms.Button butOk;
+
+        private GMap.NET.WindowsForms.GMapControl map;
+
+        #endregion
+    }
+}
---
+++ kk4/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using Newtonsoft.Json.Linq;
+
+namespace kk4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public struct coord
+        {
+            public double lat;
+            public double lon;
+        }
+        public class Us
+        {
+            public string id;
+            public coord[] сoordinates;
+        }
+        
+        private GMarkerGoogle GetMarker(PointLatLng loc, string id, GMarkerGoogleType gMarkerGoogleType= GMarkerGoogleType.lightblue_pushpin)
+        {
+            GMarkerGoogle mapMarker = new GMarkerGoogle(loc, gMarkerGoogleType);//широта, долгота, тип маркера
+            mapMarker.ToolTip = new GMap.NET.WindowsForms.ToolTips.GMapRoundedToolTip(mapMarker);//всплывающее окно с инфой к маркеру
+            mapMarker.ToolTipText = id; // текст внутри всплывающего окна
+            mapMarker.ToolTipMode = MarkerTooltipMode.OnMouseOver; //отображение всплывающего окна (при наведении)
+            return mapMarker;
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache; //выбор подгрузки карты – онлайн или из ресурсов
+            map.MapProvider = GMap.NET.MapProviders.WikiMapiaMapProvider.Instance; //какой провайдер карт используется (в нашем случае гугл) 
+            map.MinZoom = 2; //минимальный зум
+            map.MaxZoom = 18; //максимальный зум
+            map.Zoom = 4; // какой используется зум при открытии
+            map.Position = new GMap.NET.PointLatLng(66.4169575018027, 94.25025752215694);// точка в центре карты при открытии (центр России)
+            map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter; // как приближает (просто в центр карты или по положению мыши)
+            map.CanDragMap = true; // перетаскивание карты мышью
+            map.DragButton = MouseButtons.Left; // какой кнопкой осуществляется перетаскивание
+            map.ShowCenter = false; //показывать или скрывать красный крестик в центре
+            map.ShowTileGridLines = false; //показывать или скрывать тайлы
+            map.NegativeMode = false;
+            map.ShowTileGridLines = false;
+        }
+        static PointLatLng GetLocationByIP(string ipAddress)
+        {
+            IPAddress address;
+            if (IPAddress.TryParse(ipAddress, out address))
+            {
+                var request = WebRequest.Create($"https://ipinfo.io/{ipAddress}/json");
+                using (var response = request.GetResponse())
+                using (var stream = response.GetResponseStream())
+                {
+                    dynamic data = null;
+                    using (var reader = new System.IO.StreamReader(stream))
+                    {
+                        string json = reader.ReadToEnd();
+                        data = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                    }
+
+                    if (data != null && data.loc != null)
+                    {
+                        var coords = data.loc.ToString().Split(',');
+                        double lat = double.Parse(coords[0], System.Globalization.CultureInfo.InvariantCulture);
+                        double lng = double.Parse(coords[1], System.Globalization.CultureInfo.InvariantCulture);
+                        return new PointLatLng(lat, lng);
+                    }
+                }
+            }
+            return PointLatLng.Empty;
+        }
+        
+        static dynamic GetLocationByIPApi(IPAddress ipAddress)
+        {
+            string url = $"http://api.ipapi.com/api/{ipAddress}?access_key=e690a9c67b8ee75a5be4334c115349f3"; 
+
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+            try
+            {
+                string json = client.DownloadString(url);
+                return JObject.Parse(json);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            
+
+            
+
+            /*string city = (string)data["city"];
+            string country = (string)data["country_name"];
+            double latitude = (double)data["latitude"];
+            double longitude = (double)data["longitude"];
+
+            Console.WriteLine($"Город: {city}");
+            Console.WriteLine($"Страна: {country}");
+            Console.WriteLine($"Координаты: {latitude}, {longitude}");*/
+        }
+        
+        static dynamic GetLocation(IPAddress ipAddress)
+        {
+            var request = WebRequest.Create($"https://ipinfo.io/{ipAddress}/json");
+            using (var response = request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            {
+                    dynamic data = null;
+                    using (var reader = new System.IO.StreamReader(stream))
+                    {
+                        string json = reader.ReadToEnd();
+                        data = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                    }
+
+                    return data;
+            }
+        }
+        private void butOk_Click(object sender, EventArgs e)
+        {
+            map.Overlays.Clear();
+            IPAddress address;
+            GMapOverlay routes = new GMapOverlay("routes");
+            List<PointLatLng> points = new List<PointLatLng>();
+            if (IPAddress.TryParse(tbIP.Text, out address))
+            {
+                GMapOverlay gMapMarkers = new GMapOverlay("route");// создание именованного слоя
+                string targetIP = tbIP.Text;
+                var pingSender = new Ping();
+                var options = new PingOptions();
+                options.DontFragment = true;
+                options.Ttl = 1;
+                const int timeout = 1000;
+                string dat = "pingdata";
+                byte[] buffer = Encoding.ASCII.GetBytes(dat);
+                var sw = new Stopwatch();
+                while (true)
+                {
+                    sw.Reset();
+                    sw.Start();
+                    PingReply reply = null;
+                    try
+                    {
+                        reply = pingSender.Send(targetIP, timeout, buffer, options);
+                    }
+                    catch (Exception ex)
+                    {
+                        tbOut.AppendText(ex.Message);
+                        break;
+                    }
+                    sw.Stop();
+
+                    if (reply.Status == IPStatus.TimedOut)
+                    {
+                        tbOut.AppendText("Request timed out.\r\n");
+                        break;
+                    }
+
+                    if (reply.Address == null)
+                    {
+                        options.Ttl++;
+                        continue;
+                    }
+                    //dynamic data = GetLocation(reply.Address);
+                    dynamic d = GetLocationByIPApi(reply.Address);
+                    PointLatLng location;
+                    tbOut.AppendText($"Hop {options.Ttl}: {reply.Address} ({sw.ElapsedMilliseconds} ms)\r\n");
+                    /*if (data.loc != null)
+                    {
+                        var coords = data.loc.ToString().Split(',');
+                        double lat = double.Parse(coords[0], System.Globalization.CultureInfo.InvariantCulture);
+                        double lng = double.Parse(coords[1], System.Globalization.CultureInfo.InvariantCulture);
+                        location = new PointLatLng(lat, lng);
+                    }*/
+                    if (d.continent_code != null)
+                    {
+                        double lat = double.Parse(d.latitude.ToString());
+                        double lng = double.Parse(d.longitude.ToString());
+                        tbOut.AppendText($"{d.latitude}");
+                        location = new PointLatLng(lat, lng);
+                    }
+                    else
+                    {
+                        options.Ttl++;
+                        continue;
+                    }
+                    points.Add(location);
+                    //string[] str = {$"{options.Ttl}", $"{data.ip}", $"{sw.ElapsedMilliseconds}",$"{data.hostname}",$"{data.city}",$"{data.region}", $"{data.org}"};
+                    string[] str = {$"{options.Ttl}", $"{d.ip}", $"{sw.ElapsedMilliseconds}",$"{reply.Address}",$"{d.city}",$"{d.region_name}", $"{d.country_name}"};
+                    grid.Rows.Add(str);
+                    gMapMarkers.Markers.Add(GetMarker(location, $"{d.ip}"));
+                    options.Ttl++;
+                    if (reply.Address.ToString() == targetIP)
+                    {
+                        tbOut.AppendText("Trace complete.\r\n");
+                        map.Position = location;
+                        MessageBox.Show("Trace complete");
+                        GMapRoute route = new GMapRoute(points, "route");
+                        route.Stroke = new Pen(Color.Red, 3);
+                        routes.Routes.Add(route);
+                        map.Overlays.Add(routes);
+                        map.Overlays.Add(gMapMarkers);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wrong ip");
+            }
+        }
+    }
+}
